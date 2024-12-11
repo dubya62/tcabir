9cca5b58094e177170028e398ee18762569767fd
dubya <dubya297@yahoo.com>
2024-12-11 12:02:09 -0600

    Started working on converting loops. Does not handle do-while loops. Still need to finish for loops also.

--- compiler/python/converter.py
+++ compiler/python/converter.py
@@ -17,8 +17,10 @@ class Converter:
         self.tokens = self.convert_breaks(self.tokens)
 
         # convert while loops to if statements and jumps
+        self.tokens = self.convert_while_loops(self.tokens)
 
         # convert for loops to if statements and jumps
+        self.tokens = self.convert_for_loops(self.tokens)
 
 
     def convert_else_ifs(self, tokens:list[Token]) -> list[Token]:
@@ -53,7 +55,6 @@ class Converter:
         return tokens
 
 
-    # TODO: handle nested loops with breaks in them
     def convert_breaks(self, tokens:list[Token]) -> list[Token]:
         i = 0
         n = len(tokens)
@@ -63,7 +64,6 @@ class Converter:
         open_braces = 0
 
         while i < n:
-            print(tokens[i], i, n)
             if tokens[i] in ["for", "while", "switch"]:
                 brace_levels.append(open_braces)
                 jump_labels.append(self.current_label)
@@ -73,7 +73,6 @@ class Converter:
             elif tokens[i] == "}":
                 open_braces -= 1
 
-                print("HERE", brace_levels, open_braces)
                 if len(brace_levels) > 0 and brace_levels[-1] == open_braces:
                     brace_levels.pop()
                     i += 1
@@ -98,6 +97,90 @@ class Converter:
                 n += 1
                 i += 1
                 continue
+
+            i += 1
+
+        return tokens
+
+
+    def convert_while_loops(self, tokens:list[Token]) -> list[Token]:
+        """
+        while (i < 10){
+        }
+        =>
+        @x:
+        if (i < 10){
+            
+            goto @x;
+        }
+
+        """
+        print(self.current_label)
+
+        i = 0
+        n = len(tokens)
+        while i < n:
+            if tokens[i] == "while":
+                tokens[i].token = "if"
+                tokens.insert(i, "@" + str(self.current_label))
+                i += 1
+                n += 1
+                tokens.insert(i, ":")
+                i += 1
+                n += 1
+                starting_index = i
+
+                braces = []
+                while i < n:
+                    if tokens[i] == "{":
+                        braces.append("{")
+                    elif tokens[i] == "}":
+                        if len(braces) == 0:
+                            fatal_error(tokens[i], "Mismatched }...")
+                        braces.pop()
+                        if len(braces) == 0:
+                            break;
+                    i += 1
+
+                tokens.insert(i, "goto")
+                i += 1
+                n += 1
+                tokens.insert(i, "@" + str(self.current_label))
+                i += 1
+                n += 1
+                tokens.insert(i, ";")
+                i += 1
+                n += 1
+                
+                self.current_label += 1
+                i = starting_index
+            i += 1
+
+        return tokens
+
+    
+    def convert_for_loops(self, tokens:list[Token]) -> list[Token]:
+        """
+        for ($TYPE i=0; i<10; i++){
+            "hello";
+        }
+        =>
+        $TYPE i=0;
+        @x:
+        if (i < 10){
+            "hello";
+            i++;
+            goto @x;
+        }
+        """
+        i = 0
+        n = len(tokens)
+
+        while i < n:
+            if tokens[i] == "for":
+                if i + 1 >= n:
+                    fatal_error(tokens[i], "Expected ( after for...")
+            
 
             i += 1
 
--- compiler/python/testing.c
+++ compiler/python/testing.c
@@ -4,10 +4,6 @@ int main(int argc, char** argv){
 
     while (argc < 2){
         break;
-        for (int i=0; i<10; i++){
-            break;
-        }
-        break;
     }
 
     return 0;
